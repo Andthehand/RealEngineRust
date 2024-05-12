d6177993f3c33f00fc093ed4ad69afe03414f0e6
Andrew DeFord <sixdefords50@gmail.com>
2024-05-11 22:51:10 -0400

    Added glfw and created a window

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,11 @@
 extern crate real_engine;
 use real_engine::Application;
+use real_engine::window;
 
 fn main() {
     let app = Application{ message: String::from("Hello RealEngine!") };
 
-    app.real_engine_print();
+    app.real_print();
+
+    window::create_window();
 }
--- real_engine/src/lib.rs
+++ real_engine/src/lib.rs
@@ -1,11 +1,11 @@
-
+pub mod window;
 
 pub struct Application {
     pub message: String,
 }
 
 impl Application {
-    pub fn real_engine_print(&self) {
-        println!("{}", &self.message);
+    pub fn real_print(&self) {
+        print!("Test")
     }
 }
